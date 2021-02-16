034ff6088eba91f024db84884b548a0027e3a433
maksim_novikov <rodgersdevelop@gmail.com>
2021-02-16 12:17:51 +0500

    add aliases for moxy strategies

--- src/main/kotlin/com/maksimnovikov/entity/MoxyStrategy.kt
+++ src/main/kotlin/com/maksimnovikov/entity/MoxyStrategy.kt
@@ -1,16 +1,22 @@
 package com.maksimnovikov.entity
 
-enum class MoxyStrategy(val className: String) {
-    ADD_TO_END_SINGLE_STRATEGY("AddToEndSingleStrategy"),
-    ADD_TO_END_SINGLE_TAG_STRATEGY("AddToEndSingleTagStrategy"),
-    ONE_EXECUTION_STATE_STRATEGY("OneExecutionStateStrategy"),
-    SKIP_STRATEGY("SkipStrategy"),
-    ADD_TO_END_STRATEGY("AddToEndStrategy");
+enum class MoxyStrategy(val className: String, val shortName: String?) {
+    ADD_TO_END_SINGLE_STRATEGY("AddToEndSingleStrategy", "AddToEndSingle"),
+    ADD_TO_END_SINGLE_TAG_STRATEGY("AddToEndSingleTagStrategy", null),
+    ONE_EXECUTION_STATE_STRATEGY("OneExecutionStateStrategy", "OneExecution"),
+    SKIP_STRATEGY("SkipStrategy", "Skip"),
+    ADD_TO_END_STRATEGY("AddToEndStrategy", "AddToEnd");
 
     override fun toString(): String = className
 
     companion object {
+        /**
+         * @return short annotation names + full annotation name
+         * */
+        fun annotationNames(): List<String> = values().mapNotNull { it.shortName } + ANNOTATION_NAME
+
         const val ANNOTATION_NAME = "StateStrategyType"
         const val PATH = "moxy.viewstate.strategy."
+        const val ALIAS_PATH = "moxy.viewstate.strategy.alias."
     }
 }
--- src/main/kotlin/com/maksimnovikov/extentions/KtNamedFunction.kt
+++ src/main/kotlin/com/maksimnovikov/extentions/KtNamedFunction.kt
@@ -6,5 +6,8 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 fun KtNamedFunction.findAnnotationByName(annotationName: String): KtAnnotationEntry? =
     annotationEntries.firstOrNull { it.shortName.toString() == annotationName }
 
+/**
+ * @return true if any of annotations for this function has the same names
+ * */
 fun KtNamedFunction.isHasAnnotation(annotationName: String): Boolean =
     annotationEntries.any { it.shortName.toString() == annotationName }
